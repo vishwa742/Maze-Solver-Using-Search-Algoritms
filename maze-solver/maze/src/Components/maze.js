44794aeabe0de83ec569462f13d473b382d3aac6
Vishwanath Ezhil <54872169+vishwa742@users.noreply.github.com>
2021-04-25 01:21:09 -0400

    updated maze layout

--- maze-solver/maze/src/Components/maze.js
+++ maze-solver/maze/src/Components/maze.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import "./maze.css";
 
 const BOARD_SIZE = 11;
-const rowSize = 10;
-const colSize = 10;
+const rowSize = 25;
+const colSize = 50;
 
 const Maze = () => {
   const [board, setBoard] = useState(
     new Array(rowSize).fill(0).map((row) => new Array(colSize).fill(0))
   );
+
+  const changeStuff = () => {
+    boardIndeces[12][1] = 1;
+    boardIndeces[12][48] = 1;
+    setBoardIndeces([...boardIndeces]);
+  };
 
   const initArray = new Array(rowSize)
     .fill(0)
@@ -19,11 +25,13 @@ const Maze = () => {
   return (
     <>
       <div className="body">
+        <button onClick={changeStuff}>Click</button>
         {board.map((row, rowIdx) => (
           <div key={rowIdx} className="test">
             {row.map((cell, cellIdx) => (
               <div
                 key={cellIdx}
+                draggable="true"
                 className={
                   boardIndeces[rowIdx][cellIdx] === 0
                     ? "inactive_class"
